de422c07600aeffde901832756bfef197498061d
beedim <91318652+beedim@users.noreply.github.com>
2024-11-19 22:32:44 +0100

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def home():
+    return "<h1>Hello, welcome to Render deployment!</h1>"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

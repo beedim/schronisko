0d3efa7b9cc86e23146d26b4a31c98ae4ae3ccb7
beedim <91318652+beedim@users.noreply.github.com>
2024-12-13 20:30:51 +0100

    my_script.py

--- my_script.py
+++ my_script.py
@@ -119,5 +119,6 @@ def run_custom():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
+    import os
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
